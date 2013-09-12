dae9d2b9c9d6cc63f768f55ebae821d3abfe81eb
yeetong wang <sunnyxsd@gmail.com>
2013-09-13 00:22:20 +0800

    add wp project.

---
+++ WP/IrregularMessage/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using IrregularMessage.Resources;
+
+namespace IrregularMessage
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
